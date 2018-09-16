d2eee29bfb42d3adc69f7ac7e707b6ad842b722a
scottshotgg <scgaydos@gmail.com>
2018-09-15 23:24:08 -0500

    initial branch commit

--- ast/ast_test.go
+++
@@ -1,5 +0,0 @@
-package ast_test
-
-// func TestNewProgramNode(t *testing.T) {
-// 	fmt.Println(ast.NewProgramNode())
-// }
--- test/output/cpp/object.expr.cpp
+++ test/output/cpp/object.expr.cpp
@@ -1,7 +1,7 @@
-#include "/home/scottshotgg/Development/go/src/github.com/scottshotgg/express/lib/defer.cpp"
-#include "/home/scottshotgg/Development/go/src/github.com/scottshotgg/express/lib/file.cpp"
-#include "/home/scottshotgg/Development/go/src/github.com/scottshotgg/express/lib/std.cpp"
-#include "/home/scottshotgg/Development/go/src/github.com/scottshotgg/express/lib/var.cpp"
+#include "/Users/sgg7269/Development/go/src/github.com/scottshotgg/express/lib/defer.cpp"
+#include "/Users/sgg7269/Development/go/src/github.com/scottshotgg/express/lib/file.cpp"
+#include "/Users/sgg7269/Development/go/src/github.com/scottshotgg/express/lib/std.cpp"
+#include "/Users/sgg7269/Development/go/src/github.com/scottshotgg/express/lib/var.cpp"
 #include <string>
 defer onExitFuncs;
 std::map<std::string, var> structMap;
@@ -14,33 +14,33 @@ int main() {
 
   var obj = {};
   obj["something"] = "here";
-  var hey_lJhhNgSQRv = {};
-  hey_lJhhNgSQRv["me"] = true;
-  hey_lJhhNgSQRv["anIntVariable"] = 69;
-  obj["hey"] = hey_lJhhNgSQRv;
+  var hey_SiuXGmICyr = {};
+  hey_SiuXGmICyr["me"] = true;
+  hey_SiuXGmICyr["anIntVariable"] = 69;
+  obj["hey"] = hey_SiuXGmICyr;
 
   var objs[] = {};
   {
-    var _mqPpsepUyT = {};
-    _mqPpsepUyT["another"] = "object";
-    objs[0] = _mqPpsepUyT;
+    var _mXjCZUZYyf = {};
+    _mXjCZUZYyf["another"] = "object";
+    objs[0] = _mXjCZUZYyf;
   }
   {
-    var obj_fpokmdBXTD = {};
-    obj_fpokmdBXTD["something"] = "here";
-    var hey_mxTvHfxcCC = {};
-    hey_mxTvHfxcCC["me"] = true;
-    hey_mxTvHfxcCC["anIntVariable"] = 69;
-    obj_fpokmdBXTD["hey"] = hey_mxTvHfxcCC;
-    objs[1] = obj_fpokmdBXTD;
+    var obj_wrvYvTzXGZ = {};
+    obj_wrvYvTzXGZ["something"] = "here";
+    var hey_aclskXftVG = {};
+    hey_aclskXftVG["me"] = true;
+    hey_aclskXftVG["anIntVariable"] = 69;
+    obj_wrvYvTzXGZ["hey"] = hey_aclskXftVG;
+    objs[1] = obj_wrvYvTzXGZ;
   }
   {
-    var obj_ggVHwgdoUI = {};
-    obj_ggVHwgdoUI["something"] = "here";
-    var hey_kSxEBcQrho = {};
-    hey_kSxEBcQrho["me"] = true;
-    hey_kSxEBcQrho["anIntVariable"] = 69;
-    obj_ggVHwgdoUI["hey"] = hey_kSxEBcQrho;
-    objs[2] = obj_ggVHwgdoUI;
+    var obj_iTmSAFmXmr = {};
+    obj_iTmSAFmXmr["something"] = "here";
+    var hey_tYmimobctm = {};
+    hey_tYmimobctm["me"] = true;
+    hey_tYmimobctm["anIntVariable"] = 69;
+    obj_iTmSAFmXmr["hey"] = hey_tYmimobctm;
+    objs[2] = obj_iTmSAFmXmr;
   }
 }
